73e624eb11c53e42cac128780c2b7c764006af4c
Brittany Duke <bhall207@icloud.com>
2018-09-10 11:12:35 -0400

    Add project files.

---
+++ Exersice5A/Cow.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exersice5A
+{
+    class Cow
+    {
+        private string name, eat, hobbies, joke;
+        
+
+        public Cow(string name, string eat, string hobbies, string joke)
+        {
+            this.name = name;
+            this.eat = eat;
+            this.hobbies = hobbies;
+            this.joke = joke;
+
+        }
+
+        public string Name()
+        {
+            return name;
+        }
+        public string Eat()
+        {
+            return eat;
+        }
+        public string Hobbies()
+        {
+            return hobbies;
+        }
+        public string Joke()
+        {
+            return joke;
+        }
+    }
+}
---
+++ Exersice5A/Farm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exersice5A
+{
+    class Farm
+    {
+        public Farm()
+        {
+            Console.WriteLine("Welcome to Olde Mcdonalds old school farm how do ya do");
+
+        }
+        public void ChickenHello(Chicken chickenLittle)
+        {
+            Console.WriteLine($"{chickenLittle.Name()}{chickenLittle.Eat()}{chickenLittle.Hobbies()}{chickenLittle.Joke()}");
+        }
+        public void LambHello(Lamb blackSheep)
+        {
+            Console.WriteLine($"{blackSheep.Name()}{blackSheep.Eat()}{blackSheep.Hobbies()}{blackSheep.Joke()}");
+        }
+        public void PigHello(Pig wilber)
+        {
+            Console.WriteLine($"{wilber.Name()}{wilber.Eat()}{wilber.Hobbies()}{wilber.Joke()}");
+        }
+        public void CowHello(Cow maryMoo)
+        {
+            Console.WriteLine($"{maryMoo.Name()}{maryMoo.Eat()}{maryMoo.Hobbies()}{maryMoo.Joke()}");
+        }
+    }
+}
